fb6dcabcf65861f34a390b7b74adbc16e497ba99
Jordan Castro <M36007@mcap.com>
2022-01-20 16:48:54 -0500

    Finish project slide in home tab

--- src/components/main/tabs/HomeTab/ProjectSlide.js
+++ src/components/main/tabs/HomeTab/ProjectSlide.js
@@ -3,9 +3,63 @@ import { styles } from "./style.ProjectSlide.css";
 export const ProjectSlide = {
     data() {
         return {
-            currentProjectImage: "https://i.imgflip.com/58r9dj.jpg"
+            captionVisible: false,
+            currentIndex: 0,
+            projects: [
+                { 
+                    image: 'https://i.imgflip.com/58r9dj.jpg',
+                    caption: `Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption`
+                },
+                { 
+                    image: 'https://www.fredrogerscenter.org/wp-content/uploads/2015/11/dark-grey-background-FRC-Grey.png',
+                    caption: `Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption`
+                },
+                { 
+                    image: 'https://wallpapercave.com/wp/wp3269246.jpg',
+                    caption: `Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption`
+                },
+                { 
+                    image: 'https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg',
+                    caption: `Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption
+                    Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption Caption`
+                }
+            ]
         };
     },
+    computed: {
+        currentImage() {
+            return this.projects[this.currentIndex % this.projects.length].image;
+        },
+        currentCaption() {
+            return this.projects[this.currentIndex % this.projects.length].caption;
+        }
+    },
+    methods: {
+        toggleCaption() {
+            this.captionVisible = !this.captionVisible;
+        },
+        startSlide() {
+            this.timer = setInterval(this.nextSlide, 6000);
+        },
+        nextSlide() {
+            this.currentIndex += 1;
+        }
+    },
+    mounted() {
+        this.startSlide();
+    },
     template: `
-    <img id="projectImage" v-bind:src="currentProjectImage" />`
+    <img id="project-image" v-bind:src="currentImage" @click="toggleCaption" />
+    <div id="project-caption" v-if="captionVisible">
+        {{ currentCaption }}
+    </div>`
 };
