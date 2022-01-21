0d9bd1f407d826032d259eb36b9f50ddd7b770c4
Jordan Castro <M36007@mcap.com>
2022-01-21 16:07:55 -0500

    Finish SportSlide Template

--- src/components/main/tabs/HomeTab/SportSlide.js
+++ src/components/main/tabs/HomeTab/SportSlide.js
@@ -3,6 +3,12 @@ import { styles } from "./style.SportSlide.css";
 export const SportSlide = {
     template: `
     <div id="collage-container">
-        
+        <img src="https://i.imgflip.com/58r9dj.jpg" />
+        <img src="https://www.fredrogerscenter.org/wp-content/uploads/2015/11/dark-grey-background-FRC-Grey.png" />
+        <img src="https://wallpapercave.com/wp/wp3269246.jpg" />
+        <img src="https://coolbackgrounds.io/images/backgrounds/white/pure-white-background-85a2a7fd.jpg" />
+        <img src="https://i.imgflip.com/58r9dj.jpg" />
+        <img src="https://www.fredrogerscenter.org/wp-content/uploads/2015/11/dark-grey-background-FRC-Grey.png" />
+        <img />
     </div>`
 };
