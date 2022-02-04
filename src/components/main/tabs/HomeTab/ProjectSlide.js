5386be84bb6aabfce6eb6f0926dd816a3c45dfb0
Jordan Castro <M36007@mcap.com>
2022-02-04 10:19:19 -0500

    fix projectslide aspect ratio

--- src/components/main/tabs/HomeTab/ProjectSlide.js
+++ src/components/main/tabs/HomeTab/ProjectSlide.js
@@ -1,11 +1,13 @@
 import mcjewelrySlide from "../../../../assets/mcjewelry/search-parameters.png";
 import weekplannerSlide from "../../../../assets/weekplanner/form.png";
 import connectfourSlide from "../../../../assets/connectfour.png";
+import curlicueBlueBackground from "../../../../assets/curlicue-blues/eos_curlicue2e.png";
 import { styles } from "./style.ProjectSlide.css";
 
 export const ProjectSlide = {
     data() {
         return {
+            imageBorder: curlicueBlueBackground,
             captionVisible: false,
             currentIndex: 0,
             projects: [
@@ -59,7 +61,12 @@ export const ProjectSlide = {
         this.startSlide();
     },
     template: `
-    <img id="project-image" v-bind:src="currentImage" @click="toggleCaption" />
+    <img 
+        id="project-image" 
+        v-bind:src="currentImage" 
+        v-bind:style="{ backgroundImage: 'url(' + imageBorder + ')' }" 
+        @click="toggleCaption" 
+    />
     <div id="project-caption" v-if="captionVisible">
         {{ currentCaption }}
     </div>`
